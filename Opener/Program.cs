61029879de67f80d6f0da3cd0fdf7f117fd9b250
Strayker <45916090+StraykerPL@users.noreply.github.com>
2025-01-07 23:26:19 +0100

    Add project files

---
+++ Opener/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Opener
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var reader = new StreamReader("websites.txt");
+            var websitesInput = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+
+            var websitesEntries = websitesInput.Split('\n').ToList();
+            var browserPath = websitesEntries[0];
+            websitesEntries.RemoveAt(0);
+
+            var websiteLinks = new StringBuilder();
+            foreach (var websiteLink in websitesEntries)
+            {
+                websiteLinks.Append(websiteLink + ' ');
+            }
+
+            Process.Start(browserPath, websiteLinks.ToString());
+
+            reader = new StreamReader("apps.txt");
+            var appsInput = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+
+            var appsEntries = appsInput.Split('\n').ToList();
+            foreach (var appsEntry in appsEntries)
+            {
+                Process.Start(appsEntry);
+            }
+        }
+    }
+}
